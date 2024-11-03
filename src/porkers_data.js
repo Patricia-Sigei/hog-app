fab7be3654b66c45b3b289614902bcc0e570a0bb
patricia <pattychela97@gmail.com>
2024-11-03 15:58:15 +0300

    Adds hog data to porker_data.js

---
+++ src/porkers_data.js
@@ -0,0 +1,54 @@
+// src/porker_data.js
+const hogs = [
+    {
+      name: "Mudblood",
+      image: "https://via.placeholder.com/150",
+      specialty: "Intelligence",
+      weight: 200,
+      greased: false,
+      highestMedal: "Gold"
+    },
+    {
+      name: "Sizzle",
+      image: "https://via.placeholder.com/150",
+      specialty: "Speed",
+      weight: 150,
+      greased: true,
+      highestMedal: "Silver"
+    },
+    {
+      name: "Chop",
+      image: "https://via.placeholder.com/150",
+      specialty: "Strength",
+      weight: 220,
+      greased: false,
+      highestMedal: "Bronze"
+    },
+    {
+      name: "Cruncher",
+      image: "https://via.placeholder.com/150",
+      specialty: "Agility",
+      weight: 180,
+      greased: true,
+      highestMedal: "Platinum"
+    },
+    {
+      name: "Snout",
+      image: "https://via.placeholder.com/150",
+      specialty: "Sniffing",
+      weight: 160,
+      greased: false,
+      highestMedal: "Gold"
+    },
+    {
+      name: "Pork Chop",
+      image: "https://via.placeholder.com/150",
+      specialty: "Cooking",
+      weight: 240,
+      greased: true,
+      highestMedal: "Diamond"
+    }
+  ];
+  
+  export default hogs;
+  
